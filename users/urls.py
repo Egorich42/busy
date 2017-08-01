407a35fc33e19c1f8f485b5f3530f828ee03587e
egorich42 <e.g.hutter@gmail.com>
2017-08-01 08:19:27 +0300

    "

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,6 @@
 from django.conf.urls import url
 from . import views
 
-
 urlpatterns = [
     url(r'^$', views.UsersList, name='UsersList'),
     url(r'^register/$', views.RegisterFormView.as_view()),
