c2ac7135a8bb7f725e882ffe52cbcfbcc0f5d86d
Egorich42 <e.g.hutter@gmai.com>
2017-12-26 15:23:40 +0300

    "

--- SOME/singles/ertex.py
+++ SOME/singles/ertex.py
@@ -29,74 +29,74 @@ def find_endpoint(dataset,start):
 
 start_point = find_first_point(first_dataset)
 endpoint = find_endpoint(first_dataset,start_point)
-
-
-
 interval = "{}"+str(start_point)+":"+"{}"+str(endpoint)
 
-interval.format("B","B")
-interval.format("f","f")
-interval.format("H","H")
-interval.format("I","I")
-interval.format("J","J")
-interval.format("K","K")
 
-numbers = [r[0].value for r in first_dataset.Range(interval.format("B","B"))]
-odinc_code = [r[0].value for r in first_dataset.Range(interval.format("f","f"))]
-remains_on_warehouse = [r[0].value for r in first_dataset.Range(interval.format("H","H"))]
-remains_summ= [r[0].value for r in first_dataset.Range(interval.format("I","I"))]
-coming_to_warehouse = [r[0].value for r in first_dataset.Range(interval.format("J","J"))]
-price = [r[0].value for r in first_dataset.Range(interval.format("K","K"))]
+def excol_to_list(dataset_name, col_name):
+	list_name = [r[0].value for r in dataset_name.Range(interval.format(col_name,col_name))]
+	return list_name
+	pass
 
-list_of_cols = (odinc_code,remains_on_warehouse,remains_summ,coming_to_warehouse,price)
+numbers = excol_to_list(first_dataset,"B")
+odinc_code= excol_to_list(first_dataset,"F")
+remains_on_warehouse= excol_to_list(first_dataset,"H")
+remains_summ= excol_to_list(first_dataset,"I")
+coming_to_warehouse= excol_to_list(first_dataset,"J")
+price= excol_to_list(first_dataset,"K")
+
+
+list_of_cols = (numbers,odinc_code,remains_on_warehouse,remains_summ,coming_to_warehouse,price)
 
 def get_warehouse_data():
 	list_length = list(range(len(odinc_code))) 
 	warehouse_update_list=[]
+	codes =[]
+	prices =[]
+	comings_to_stocs = []
+
+	new_codes =[]
+	new_prices =[]
+	new_comings_to_stocs = []
+
 	for i in list_length:
-		if (list_of_cols[3][i] != 0.0 and list_of_cols[3][i] != None and list_of_cols[0][i] != None):
-			warehouse_data_dict = {'1C_код':list_of_cols[0][i], 'остаток на складе':list_of_cols[1][i],'сумма остатка':list_of_cols[2][i], 'приход на склад':list_of_cols[3][i],'цена': list_of_cols[4][i]}
+		if (list_of_cols[4][i] != 0.0 and list_of_cols[4][i] != None and list_of_cols[0][i] != None and list_of_cols[1][i] != None):
+			warehouse_data_dict = {'номер':list_of_cols[1][0], '1C_код':list_of_cols[1][i], 'остаток на складе':list_of_cols[2][i],'сумма остатка':list_of_cols[3][i], 'приход на склад':list_of_cols[4][i],'цена': list_of_cols[5][i]}
 			warehouse_update_list += [warehouse_data_dict]
-		pass
-	return warehouse_update_list
-	pass
-
-get_warehouse_data()
-print(len(get_warehouse_data()))
-
-"""
 
 
-def create_all_lists():
-	rent_parameters = create_first_arenda_list()
-	rent_list=[]
-	mnojitel = [0.1984, 0.1522, 0.1111]
-	for m in mnojitel:
-		arendator_price = [round(i['price']*m, 2) for i in rent_parameters if type(i['price'])==float]
+	for i in warehouse_update_list:
+		if i['остаток на складе'] != None:
+			if i['остаток на складе']* i['цена'] == i['сумма остатка']:
+				codes +=[i['1C_код']]
+				prices +=[i['цена']]
+				comings_to_stocs += [i['приход на склад']]	
 
-		sum_price = round(sum(arendator_price),2)
 
-		rent_list +=[([i for i in arendator_price],(sum_price))]
 
-	return rent_list
+			if i['остаток на складе']* i['цена'] != i['сумма остатка']:
+				new_codes+=[i['1C_код']]
+				new_prices +=[i['сумма остатка']/i['остаток на складе']]
+				new_comings_to_stocs += [i['приход на склад']]
+
+		
+	return (warehouse_update_list,codes,prices,comings_to_stocs,new_codes,new_prices,new_comings_to_stocs)
 	pass
 
 
-names_list = [i['name'] for i in create_first_arenda_list()]
-arenda = [create_all_lists()[i][0] for i in (0,1,2)]
 
 
+codes = get_warehouse_data()[1]
+prices = get_warehouse_data()[2]
+comings = get_warehouse_data()[3]
 
-#http://www.py-my.ru/post/4e15588e1d41c81105000003
-
-first_list = Excel.Workbooks.Open(BASE_DIR+'transkom_in.xls')
-first_dataset = first_list.ActiveSheet
-
+new_codes = get_warehouse_data()[4]
+new_prices = get_warehouse_data()[5]
+new_comings = get_warehouse_data()[6]
 
 #записываем последовательность
-def import_into_excel(document_name, names, arenda):
+def import_into_excel(document_name, *args):
 	book = xlwt.Workbook('utf8')
-	sheet = book.add_sheet('аренда')
+	sheet = book.add_sheet('поступление на склад')
 
 	sheet.portrait = False
 
@@ -104,16 +104,26 @@ def import_into_excel(document_name, names, arenda):
 	created_book = book.save(document_name)
 	active_doc = Excel.Workbooks.Open(BASE_DIR+document_name)
 	active_sheet = active_doc.ActiveSheet
+	active_sheet.Cells(2,2).value ="test"
 
-	i = 3
-	for rec in names:
-		active_sheet.Cells(i,2).value = rec
-		i = i + 1
+	a = 3
+	active_sheet.Cells(2,1).value ="Код в 1С"
+	for code in args[0]:
+		active_sheet.Cells(a,1).value = code
+		a = a + 1
 
-	m = 30
-	for sec in arenda:
-		active_sheet.Cells(m,3).value = sec
-		m = m + 1
+	b = 3
+	active_sheet.Cells(2,2).value ="Приход за месяц"
+	for coming in args[1]:
+		active_sheet.Cells(b,2).value = coming
+		b = b + 1
+
+
+	c = 3
+	active_sheet.Cells(2,3).value ="Цена за единицу"
+	for price in args[2]:
+		active_sheet.Cells(c,3).value = price
+		c = c + 1
 
 
 	active_doc.Save()
@@ -121,9 +131,8 @@ def import_into_excel(document_name, names, arenda):
 	Excel.Quit()
 	pass
 
-import_into_excel('ertex_out.xls',names_list, arenda[2])
+#import_into_excel('ertex_out.xls',codes,comings,prices)
+import_into_excel('ertex_out_new.xls',new_codes,new_comings,new_prices)
 
-
-"""
 
 #сохраняем рабочую книгу
