c316c34cc4987fe7754f63872be369b7300c8c65
Egorich42 <e.g.hutter@gmai.com>
2018-01-20 15:51:58 +0300

    "

--- SOME/singles/ertex/ertex.py
+++ SOME/singles/ertex/ertex.py
@@ -3,13 +3,13 @@
 import win32com.client
 import os
 import xlwt
-
+import numpy as np
 
 Excel = win32com.client.Dispatch("Excel.Application")
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))+'\\'
 
-first_list = Excel.Workbooks.Open(BASE_DIR+'ertex_in_dec.xls')
+first_list = Excel.Workbooks.Open(BASE_DIR+'ertex_in_oct.xls')
 first_dataset = first_list.ActiveSheet
 
 
@@ -33,7 +33,7 @@ interval = "{}"+str(start_point)+":"+"{}"+str(endpoint)
 
 
 def excol_to_list(dataset_name, col_name):
-	list_name = [r[0].value for r in dataset_name.Range(interval.format(col_name,col_name))]
+	list_name = np.array([r[0].value for r in dataset_name.Range(interval.format(col_name,col_name))])
 	return list_name
 	pass
 
@@ -47,6 +47,8 @@ price= excol_to_list(first_dataset,"K")
 
 list_of_cols = (numbers,odinc_code,remains_on_warehouse,remains_summ,coming_to_warehouse,price)
 
+
+"""
 def get_warehouse_data():
 	list_length = list(range(len(numbers))) 
 
@@ -58,6 +60,7 @@ def get_warehouse_data():
 
 	new_codes =[]
 	new_prices =[]
+	old_prices =[]
 	new_comings_to_stocs = []
 
 	for i in list_length:
@@ -65,35 +68,73 @@ def get_warehouse_data():
 			warehouse_data_dict = {'номер':list_of_cols[1][0], '1C_код':list_of_cols[1][i], 'остаток на складе':list_of_cols[2][i],'сумма остатка':list_of_cols[3][i], 'приход на склад':list_of_cols[4][i],'цена': list_of_cols[5][i]}
 			warehouse_update_list += [warehouse_data_dict]
 
-
+			
 	for i in warehouse_update_list:
 		codes +=[i['1C_код']]
 		prices +=[i['цена']]
-		comings_to_stocs += [i['приход на склад']]	
+		comings_to_stocs += [i['приход на склад']]
+		remains_on_stock +=	
 	
-
-
 
 		if i['остаток на складе'] != None:
 			if i['сумма остатка']/i['остаток на складе'] != i['цена']:
 				new_codes+=[i['1C_код']]
-				new_prices +=[i['сумма остатка']/i['остаток на складе']]
+				new_prices +=[i['цена']]
+				old_prices += [round(i['сумма остатка']/i['остаток на складе'],2)]
 				new_comings_to_stocs += [i['приход на склад']]		
 
 		
-	return (warehouse_update_list,codes,prices,comings_to_stocs,new_codes,new_prices,new_comings_to_stocs)
+	return (codes,prices,comings_to_stocs,new_codes,new_prices,new_comings_to_stocs,old_prices)
+	pass
+
+"""
+
+
+
+def get_warehouse_data():
+	list_length = list(range(len(numbers))) 
+
+	warehouse_update_list=[]
+
+	codes =[]
+	prices =[]
+	comings_to_stocs = []
+	remains_on_stock =[]
+	remains_summ  =[]
+
+
+	for i in list_length:
+		if (list_of_cols[4][i] != 0.0 and list_of_cols[4][i] != None and list_of_cols[0][i] != None and list_of_cols[1][i] != None):
+			warehouse_data_dict = {'номер':list_of_cols[1][0], '1C_код':list_of_cols[1][i], 'остаток на складе':list_of_cols[2][i],'сумма остатка':list_of_cols[3][i], 'приход на склад':list_of_cols[4][i],'цена': list_of_cols[5][i]}
+			warehouse_update_list += [warehouse_data_dict]
+
+			
+	for i in warehouse_update_list:
+		codes +=[i['1C_код']]
+		prices +=[i['цена']]
+		comings_to_stocs += [i['приход на склад']]
+		remains_on_stock +=	[i['остаток на складе']]
+		remains_summ +=[i['сумма остатка']]
+	
+	return (codes,prices,comings_to_stocs,remains_on_stock,remains_summ)
 	pass
 
 
 
 
-codes = get_warehouse_data()[1]
-prices = get_warehouse_data()[2]
-comings = get_warehouse_data()[3]
+transformed_warehouse_data = np.array(get_warehouse_data())
 
-new_codes = get_warehouse_data()[4]
-new_prices = get_warehouse_data()[5]
-new_comings = get_warehouse_data()[6]
+#print(transformed_warehouse_data.shape)
+
+codes = transformed_warehouse_data[0]
+prices = transformed_warehouse_data[1]
+comings = transformed_warehouse_data[2]
+
+new_codes = transformed_warehouse_data[3]
+new_prices = transformed_warehouse_data[4]
+new_comings = transformed_warehouse_data[5]
+old_prices = transformed_warehouse_data[6]
+
 
 #записываем последовательность
 def import_into_excel(document_name, *args):
@@ -121,15 +162,19 @@ def import_into_excel(document_name, *args):
 		b = b + 1
 
 
+
 	c = 3
-	if args[2] == new_prices:
-		active_sheet.Cells(2,3).value ="Новая цена"
-	else: 
-		active_sheet.Cells(2,3).value ="Цена за единицу"	
-		pass
-	for price in args[2]:
-		active_sheet.Cells(c,3).value = price
+	active_sheet.Cells(2,3).value ="Приход за месяц"
+	for coming in args[2]:
+		active_sheet.Cells(c,3).value = coming
 		c = c + 1
+
+	d = 3
+	active_sheet.Cells(2,4).value ="Приход за месяц"
+	for coming in args[3]:
+		active_sheet.Cells(d,4).value = coming
+		d = d + 1
+
 
 
 	active_doc.Save()
@@ -138,7 +183,4 @@ def import_into_excel(document_name, *args):
 	pass
 
 #import_into_excel('ertex_out.xls',codes,comings,prices)
-import_into_excel('ertex_out_new.xls',new_codes,new_comings,new_prices)
-
-
-#сохраняем рабочую книгу
+import_into_excel('ertex_out_new.xls',new_codes,new_comings,new_prices,old_prices)
